91c5b579b4a93146f11d2724c558d2bd8d151bf9
jesusreales19 <jesusrealescastro3@gmail.com>
2026-03-19 23:50:52 -0500

    segundo commit de Individuo

--- src/test/java/com/project/ProjectApplicationTests.java
+++ src/test/java/com/project/ProjectApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Febrero27ApplicationTests {
+class ProjectApplicationTests {
 
 	@Test
 	void contextLoads() {
